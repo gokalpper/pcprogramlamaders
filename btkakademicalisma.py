5d6c4c8d20308ae4f2238814ba1facde52bdd9cb
gokalpper <gokalp.er@hotmail.com>
2022-11-22 21:43:35 +0300

    Create btkakademicalisma.py

---
+++ btkakademicalisma.py
@@ -0,0 +1,76 @@
+# print("merhaba")
+
+# NUMBERS: SAYI VERİ TİPLERİ
+
+# print(3+5)
+
+#  NUMBERS:MATHS OPERATION 
+
+# print(2**3 )
+# print(11//3)     tam sayı kısmını yazıyor
+# print(10/3)     bütün sonucu yazıyor   
+# print(10%2)    kalanın kaç olduğunu gösterir
+# print(11%2)      #kalanın 1 olduğunu gösterir.
+
+# DEĞİŞKEN TANIMLAMA (variable)
+
+#print(5000 - (5000 * 0.27) )     #brüt maaş hesaplama = alınan maas 5k %yüzde 27 kesilen vergi sonucu ne kadar para alır
+#print(17000 - (5000 * 0.18) )
+
+#maasAli = 5000
+#maasgok = 10000
+#vergi = 0.27 
+
+#print(maasAli-(maasAli * vergi))
+#print(maasgok-(maasgok * vergi))
+
+# DEĞİŞKEN TANIMLAMA KURALLARI 
+
+# DEĞİŞKENLER ARASINDA BOŞLUK OLAMAZ _ OLABİLİR.
+
+#rakam ile başlayamaz 
+#ÖR:
+#number = 10  number1 = 10   _number = 10 # BUNLAR OLABİLİR
+#1number = 10 # BUNLAR OLAMAZ
+
+number = 10
+print(number)
+
+number = 20
+
+number += 30 
+print(number)
+
+# BÜYÜK KÜÇÜK HARF DUYARLILIĞI 
+
+age = 20
+AGE = 30 
+
+print(age)
+print(AGE)
+
+#türkçe karakter kullanamyın 
+
+yas = 20 
+_age = 20 
+
+x = 1                      # int  
+y = 2.3                    # float
+name = "Çınar"             # string
+isStudent = True           # bool
+
+# x, y , name, isStudent = ( 1, 2.3, "Çınar", True)
+
+a = 10 
+b = 20 
+print(a+b) # 30 
+
+a = '10'
+b = '20'
+print(a+b) #1020      #sonuç: buna da Satring birleştirme deniyor.
+
+firstName = "Gökalp"
+lastname = "ER" 
+
+print(firstName + lastname ) # sonuç: GökalpER buna da STRİNG birleştime deniyor
+
