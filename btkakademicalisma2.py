beac7e970f394d5fef66cac2f92a79d2dedec567
gokalpper <gokalp.er@hotmail.com>
2022-11-23 17:53:17 +0300

    Create btkakademicalisma2.py

---
+++ btkakademicalisma2.py
@@ -0,0 +1,30 @@
+#degisken tanımlama 
+
+musteriAdi = "Ali"
+musteriSoyad = "Yılmaz"
+musteriAdSyoad = musteriAdi + ' ' + musteriSoyad
+print(musteriAdSyoad) 
+
+# musteriCinsiyet = True # Erkek 
+# musteriTcKimlik = '123456789465'
+# musteriDogumYili = '1989'
+# musteriAdresi = 'Istanbul Kadikoy'
+# musteriYasi = 2019 - musteriDogumYili
+
+# print(musteriYasi)  
+# 2 - SİPARİŞ TOPLAM BİLGİSİ
+
+#SİPARİŞ 1 => 110 TL
+#SİPARİŞ 2 => 1100.5 TL
+#SİPARİŞ 3 => 356.95 TL
+
+order1 = 110 
+order2 = 1100.5
+order3 = 356.95
+
+total = order1 + order2 + order3
+
+print("Total:" , total) 
+
+
+
